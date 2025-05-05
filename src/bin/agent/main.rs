62999d1df60c124e6297541a3e81433ac17670ee
Kira Sotnikov <s8tn1kv@gmail.com>
2025-05-05 19:52:57 +0300

    v0.1.7

--- src/bin/agent/main.rs
+++ src/bin/agent/main.rs
@@ -10,7 +10,7 @@ mod core;
 #[derive(Parser)]
 #[command(
     about = "Pony Agent - control tool for Xray/Wireguard",
-    version = "v0.1.5"
+    version = "v0.1.7"
 )]
 struct Cli {
     #[arg(short, long, default_value = "config.toml")]
